c64ffae26ccc6439d840598c805c88591ecceeb8
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-04-28 09:33:58 +0100

    correct typo

--- checkAuroracam.sh
+++ checkAuroracam.sh
@@ -15,7 +15,7 @@ do
             ping -c 1  -w 1 $IPADDRESS > /dev/null 2>&1
             if [ $? -eq 1 ] ; then 
                 logger -s -t checkAuroracam "no response, trying to reset IP address"
-                python $here/camManager.py "search;config $MACADDRESS $IPADDRESS 255.255.255.0 192.168.1.1;quit"
+                python $here/CamManager.py "search;config $MACADDRESS $IPADDRESS 255.255.255.0 192.168.1.1;quit"
             else
                 logger -s -t checkAuroracam "camera ok, likely software failure, restarting"
                 systemctl --user restart auroracam
