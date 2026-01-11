15075f256eb2ad4b594d9b14524d31a9e0257c0d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-01-11 16:29:19 +0000

    use rms-mqtt for logging

--- logToMQTT.sh
+++ logToMQTT.sh
@@ -5,5 +5,5 @@
 here="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ~/vAuroracam/bin/activate
 
-cd $here
-python -c "from auroraCam import sendToMQTT ; sendToMQTT() ; "
+cd $here/../rms_mqtt
+python -c "from sendToMQTT import sendOtherData;sendOtherData();"
